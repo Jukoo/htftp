1aa6930f1fd9b197254a5ad7bdf1443d35b14424
jukoo <jUmarB@protonmail.com>
2024-12-16 08:03:40 +0000

    allow navigation between  directory but 1 depth level. But Code quality regression need to fix it

--- htftp.c
+++ htftp.c
@@ -16,9 +16,9 @@
 
 struct http_protocol_header_t 
 {
-  char *method ; 
+  char *method; 
   char *request; 
-  char *http_version; 
+  char *http_version;  
 }; 
 
 struct http_request_header_t 
@@ -55,27 +55,62 @@ http_reqhdr_t  *parse_http_request(char http_rbuff __parmreq)
       *(header+i) = strdup(s) ; 
       rbuff  = n+1;   
       i=-~i ;  
-   } 
-
-   (void *)explode(&hrq->http_hproto ,  *(header+0) ); 
+   }  
+   
+   if (*(header+0) == nptr)  
+    return  hrq ;   
+   
+   __maybe_unused explode(&hrq->http_hproto ,  *(header+0) ); 
+ 
+   puts("dsad") ;  
    hrq->server_host  = strdup(*(header+1)); 
    hrq->user_agent   = strdup(*(header+2)); 
-
+   puts("endsss") ; 
    release_local_alloc(header)  ; 
    return  hrq ; 
 }
 
-static http_protocol_header_t  * explode(http_protocol_header_t *hproto , char raw_data __parmreq)  
+static http_protocol_header_t  * explode(http_protocol_header_t *hproto , char *raw_data)  
 {
-  char chunck[HTTP_REQUEST_HEADER_LINE][0xff] = {0} ; 
 
-  sscanf(raw_data , "%s  %s  %s",  
-         hproto->method  = strdup((char*)(chunck+METHOD)),
-         hproto->request = strdup((char*)(chunck+RESOURCE)),  
-         hproto->http_version = strdup((char*)(chunck+VERSION)) ) ;
+  puts("--> bug here sscanf ") ;
+  static char chunck[HTTP_REQUEST_HEADER_LINE][0xff] = {0} ; 
+  printf("data ->   %s\n" , raw_data==nptr? "yeah is null" : "not null") ;   
+  if (raw_data ==  nptr)
+  {
+    puts("returning null") ; 
+    hproto->method       = strdup((char*)(chunck+METHOD));
+    hproto->request      = strdup((char*)(chunck+RESOURCE));
+    hproto->http_version = strdup((char*)(chunck+VERSION)) ; 
+    printf("mthode %s \n" , hproto->method) ; 
+    printf("resources  %s \n" , hproto->request) ; 
+    printf("http_version %s \n" , hproto->http_version) ; 
+    return hproto ;  
 
-  //!TODO : check  hproto  members  contains data ;  
-  return hproto ; 
+  }
+
+  if (raw_data != nptr)  
+  {
+    
+    printf("--------- %s \n" , raw_data) ; 
+    sscanf(raw_data , "%s  %s  %s", (chunck+METHOD) , (chunck+RESOURCE) , (chunck+VERSION)) ; 
+
+    hproto->method       = strdup((char*)(chunck+METHOD));
+    hproto->request      = strdup((char*)(chunck+RESOURCE));
+    hproto->http_version = strdup((char*)(chunck+VERSION)) ; 
+
+    puts("xxxxxxxxx")  ; 
+    printf("mthode %s \n" , hproto->method) ; 
+    printf("resources  %s \n" , hproto->request) ; 
+    printf("http_version %s \n" , hproto->http_version) ; 
+    //!TODO : check  hproto  members  contains data ;  
+    puts("--> bug here  sscanf end ") ; 
+
+    return hproto ; 
+  }
+
+  return nptr ; 
+ 
 }
 
 //! freeing resources because strdup function 
@@ -91,9 +126,11 @@ static void  release_local_alloc(char  **_arr)
 } 
 
 //! retrieve  the requested content from http_request_header_t 
+//! GET  the requested resource  from user agent 
 char *http_get_requested_content(http_reqhdr_t *http_req)   
 {
   char *requested_filename  = http_get_RESOURCE(http_req)  ; 
+  printf("|||||>requested resources  %s \n" , requested_filename) ; 
   char *http_default_hypertext =  HTTP_HYPERTEXT_DEFAULT_FNAME  ; 
 
   //!  looking for / only that mean it try  to reach "index.html"  
@@ -104,7 +141,8 @@ char *http_get_requested_content(http_reqhdr_t *http_req)
 
     return   strdup(http_default_hypertext) ; 
   } 
-  
+ 
+  //!TODO :  if is another file  different thant the index.html one enable downloading 
   requested_filename =  (requested_filename+1) ;  
   
   struct stat stbuff ;  
@@ -151,52 +189,55 @@ char * http_query(http_reqhdr_t *  http_req  , int section)
    return  data ;  
 }
 
-
-char * http_read_content(char *filename ) 
+//!  Read  the asked ressource 
+char * http_read_content(char *filename , char *content_dump)  
 {
   char content_buffer[HTTP_REQST_BUFF] =  {0} ;  
   
   if(!filename)      
   {
-    return http_list_dirent_content(nptr) ;   
+    return http_list_dirent_content(nptr , content_dump) ;   
   } 
-  char *is_dir = strchr(filename , 0x23) ; 
+  //! if found "#"
+  char *is_dir = strchr(filename , 0x23) ;  
+  
   if(is_dir)
   {
-    *is_dir = 0 ; 
-    return http_list_dirent_content(filename) ; 
+    *is_dir = 0 ;   
+    return http_list_dirent_content(filename , content_dump) ; 
   } 
   
-  //!TODO  : check if the file existe  
+  if (access(filename , F_OK| R_OK)) return  nptr ; 
+  
   int  hyper_text_fd = open(filename , O_RDONLY) ; 
-  if (~0 ==  hyper_text_fd)  
-    return nptr;  
+  if (~0 ==  hyper_text_fd)  return nptr;  
+  
+  struct   stat stbuf; 
+  size_t   requested_bsize = ~0 != fstat(hyper_text_fd , &stbuf)  ? stbuf.st_size  :  HTTP_REQST_BUFF ; 
+  size_t   rbyte =  read(hyper_text_fd ,content_buffer , requested_bsize)  ; 
 
-  size_t rbyte =  read(hyper_text_fd ,content_buffer , HTTP_REQST_BUFF)  ; 
   assert(!rbyte^(strlen(content_buffer))) ; 
   close(hyper_text_fd) ; 
   
-  return strdup(content_buffer) ; 
-  
+  memcpy(content_dump , content_buffer ,HTTP_REQST_BUFF) ;   
+  return  content_dump ; 
 }
 
 
 int http_transmission(int  user_agent_fd   ,  char  content_delivry __parmreq) 
 {
-  //!NOTE : Change it 
-  char content_buffer[HTTP_REQST_BUFF] = HTTP_HEADER_RESPONSE_OK;   
+  //!NOTE : Change it  
+  char content_buffer[HTTP_REQST_BUFF] = {0} ; 
   
   http_prepare(content_buffer,HTTP_HEADER_RESPONSE_OK
                                , content_delivry 
                                , STR(CRLF)) ; 
-  
-  //preappend(content_buffer , "%s%s%s", HTTP_HEADER_RESPONSE_OK ,content_delivry ,STR(CRRL))
-  //strcat(content_buffer,   content_delivry) ; 
-  //strcat(content_buffer,  STR(CRLF))   ; 
+ 
   ssize_t content_bsize  = strlen(content_buffer) ;  
-  
-  ssize_t s = send(user_agent_fd , content_buffer , sizeof(content_buffer) ,  __fignore);  
-  return  s^sizeof(content_buffer)  ; 
+  //!use sendfile  if the file is  not index.html   
+  ssize_t sbytes= send(user_agent_fd , content_buffer , sizeof(content_buffer) ,  __fignore);  
+ 
+  return  sbytes^sizeof(content_buffer)  ; 
   
 }
 void clean_http_request_header(int status_code ,  void * hrd)  
@@ -219,29 +260,37 @@ void clean_http_request_header(int status_code ,  void * hrd)
   hrq=nptr;  
 } 
 
-static char * http_list_dirent_content(char  dir __parmreq)   
+static char * http_list_dirent_content(char  dir __parmreq  , char * dumper )   
 {
-   
-  // __reseterrno ;  
+  
+ 
   errno = 0 ; 
-  char current_dirent[PATH_MAX_LENGHT] = {0} ; 
-  __maybe_unused getcwd(current_dirent , PATH_MAX_LENGHT) ;  
+  static int  active_trace_path =0 ; 
+  static char  trace_path[PATH_MAX_LENGHT] ={0} ;  
+  
+  char current_dirent_root[PATH_MAX_LENGHT] = {0} ; 
+  __maybe_unused getcwd(current_dirent_root , PATH_MAX_LENGHT) ;  
 
-  if ( 0 != errno &&  (ERANGE & errno)) 
+  if ( 0 != errno && (ERANGE & errno)) 
   {
      warn("Total lenght Path Exceeded %s\n",   strerror(*__errno_location())); 
+     return nptr; 
   }
+ 
 
+  char subdir[PATH_MAX_LENGHT] = {0} ; 
   if(dir)  
-  {
-    char subdir[PATH_MAX_LENGHT] = {0} ; 
+  { 
+    //! NOTE : dealing with navigation between path 
     sprintf(subdir , "/%s" , dir) ; 
-    strcat(current_dirent ,  subdir) ; 
+    strcat(current_dirent_root ,  subdir);  
+
+    printf("path -> %s \n" , current_dirent_root) ; 
   }
    
   char  http_dom_content[HTTP_REQST_BUFF] = HTTP_DIRENDER_DOCTYPE; 
   
-  DIR *dirent  = opendir(current_dirent) ;  
+  DIR *dirent  = opendir(current_dirent_root) ;  
   if (!dirent) 
   {
      fprintf(stderr ,  "Not Able to open directory  content: %s\n", strerror(*__errno_location())) ;  
@@ -252,26 +301,24 @@ static char * http_list_dirent_content(char  dir __parmreq)
 
   while ( (dirent_scaner = readdir(dirent)) != nptr)    
   {  
-    /**  
-     * NOTE: Activate  limit 
-     * if (max_limite) // break loop  
-     * 
-     */
-    //! Apply filter on directory  list only  regular common file  
+    //! Apply filter on directory  list only  regular  and  common file  
     //! not special  files 
     if(dirent_scaner->d_type & (DT_REG | DT_DIR))
     {  
-      hypertex_http_dom_append2list(dirent_scaner->d_name, http_dom_content ) ;  
+      hypertex_http_dom_append2list(dirent_scaner->d_name, http_dom_content , subdir) ;  
       //!  
-      if (10 == i)break ; 
-      i=-~i ;
+      //if (10 == i)break ; 
+      //i=-~i ;
     }
   } 
   
   strcat(http_dom_content,HTTP_DIRENDER_DOCTYPE_END) ; 
+  
+  memcpy(dumper, http_dom_content , strlen(http_dom_content)) ;  
+  return dumper; 
 
-  return  strdup(http_dom_content) ;  
 }
+
 
 static void  http_prepare(char __global_content  __parmreq, ...)
 {
@@ -285,7 +332,6 @@ static void  http_prepare(char __global_content  __parmreq, ...)
    {
      char *item =  va_arg(ap , char*) ; 
      memcpy((__global_content+offset) , item , strlen(item)) ; 
-     //!update the offset ; 
      offset = strlen(__global_content) ; 
    }
 
