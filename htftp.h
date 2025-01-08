aac432e1f078b7ced36d76f65aa210411d6d14c1
jukoo <jUmarB@protonmail.com>
2025-01-08 17:32:36 +0000

    wrap it up like external lib

--- htftp.h
+++ htftp.h
@@ -14,10 +14,13 @@
 #define  __null  (__ptr_t) 0  
 #define  __bunit  1<<3 
 #define  nptr  __null
-
 #define  _Nullable  
 
-#define __maybe_unused (__ptr_t) 
+#ifdef  __unused__ 
+# define __maybe_unused __unused__ 
+#else   
+# define __maybe_unused 
+#endif 
 
 #if !defined(CND_FOR_STYLESHEET) 
 # define  CND_FOR_STYLESHEET "https://unpkg.com/98.css" 
@@ -87,9 +90,10 @@ enum {
 #define  hget(__item)  \
   http_get_##__item(http_reqhdr_t *) 
 
+#if !defined(LISTEN_BACKLOG) 
+# define LISTEN_BACKLOG  4 
+#endif 
 
-#define LISTEN_BACKLOG  4 
-#define CONFIGURE_LOCAL {PF_INET, htons(DEFAULT_PORT) ,htonl(INADDR_ANY) }   
 
 //! Only the 3 first  GET , HOST , USER-AGENT 
 #define HTTP_REQUEST_HEADER_LINE  3 
@@ -114,15 +118,15 @@ enum {
 
 #define __habsp  (path+1)  
 
-#define  check(__return_code , fcall)       \
-  if(~0 == __return_code)                   \
-    errx(~0, #fcall": %s", strerror(errno)) 
+#define  HTFTP_DEFCFG {PF_INET, htons(DEFAULT_PORT) ,htonl(INADDR_ANY)}   
+#define __getsockfd(__hf)  ((__hf->_sp) >> 0x10) 
+#define __getportnb(__hf)  ((__hf->_sp) &  0xffff)   
 
-#define  SAi struct sockaddr_in 
 #define  SA  struct sockaddr 
 
-typedef  struct http_protocol_header_t  http_protocol_header_t ; 
-typedef  struct http_request_header_t   http_reqhdr_t ; 
+typedef  struct __http_protocol_header_t  http_protocol_header_t ; 
+typedef  struct __http_request_header_t   http_reqhdr_t ; 
+typedef  struct __htftp_t                 htftp_t;
 
 typedef  struct fobject_t 
 {
@@ -131,6 +135,9 @@ typedef  struct fobject_t
    size_t fsize ;    
    time_t ftime ;  
 } fobject_t ; 
+
+typedef void(*__htftp_fcfg_t)(struct __htftp_t * __restrict__ ,  void * __maybe_unused _Nullable) ; 
+#define  htftp_fcfg  __htftp_fcfg_t  
 
 #define TIME_ASC 1    //!  time read format  ascii mode    
 #define TIME_NUM 2    //!  time read format  numerical mode 
@@ -141,17 +148,27 @@ static http_protocol_header_t *explode(http_protocol_header_t * __hproto, char *
 static char *http_list_dirent_content(char *ftype ,  char  *__dump) ; 
 static void  release_local_alloc(char  **_arr);
 static void  http_prepare(char *__restrict__ __global_content , ...) ; 
+static void  setup_htftp(struct __htftp_t  *__restrict__ __hf , int __socket_fd , int __portnumber) ; 
+static void  __use_defconfig(struct __htftp_t* __restrict__ __hf , void * __maybe_unused _Nullable __xtrargs) ;
+ 
+extern void htftp_close(struct  __htftp_t * __restrict__ __hf );
+
+//!###########################################################################
+htftp_t  *htftp_start(int  __port_number ,  
+                      __htftp_fcfg_t _Nullable __function_configuration  ,  
+                      void * _Nullable  __restrict__ __extra_argument) __attribute__((weak)) ; 
+
+int  htftp_polling(struct __htftp_t  * __restrict__  __hf) ; 
 
 http_reqhdr_t* parse_http_request( char __http_buffer __parmreq); 
+char * http_get_requested_content(http_reqhdr_t * __restrict__ __hproto)  ; 
+char * http_read_content(char *__restrict__ __filename , char * __restrict__  __dump) ; 
+int http_transmission(int  __user_agent   ,  char  __content_delivry __parmreq) ; 
 
-char * http_get_requested_content(http_reqhdr_t *__hproto)  ; 
-char * http_read_content(char *__filename , char * __dump) ; 
+fobject_t * file_detail(fobject_t *__restrict__  __fobj ,  char *__restrict__ __fitem , int __tfmtopt ) ;
+static char * file_size_human_readable(float __raw_filesize) ; 
 
-int http_transmission(int  __user_agent   ,  char  content_delivry __parmreq) ; 
 
-fobject_t * file_detail(fobject_t *  __fobj ,  char *__fitem , int __tfmtopt ) ;
-
-static char * file_size_human_readable(float raw_filesize) ; 
 __extern_always_inline void  append2tablerow(char item __parmreq,
                                       char render_buffer  __parmreq, 
                                       char * subdirent _Nullable , 
--- main_htftp.c
+++ main_htftp.c
@@ -12,72 +12,50 @@
 #include <assert.h> 
 #include <fcntl.h> 
 #include <poll.h> 
+#include <error.h> 
 
 
 #include "htftp.h" 
 
-//! NO acknowlegment 
-#define NOACK nptr,nptr 
 
 int
 main(int ac , char **av , char **env)   
 {
- 
-  //!  No buffering on stdout 
-  setvbuf(stdout, __null, _IONBF , 0) ; 
-
-  int server_socket_fd = socket(PF_INET , SOCK_STREAM, IPPROTO_TCP ); 
-  check(server_socket_fd , socket ) ;  
- 
-  SAi server = CONFIGURE_LOCAL ; 
-  socklen_t server_len=  sizeof(server);  
   
-  int always_reuseaddr=1; 
-  int ssopt   = setsockopt( server_socket_fd, SOL_SOCKET, SO_REUSEADDR,  
-                            &always_reuseaddr, sizeof(always_reuseaddr) ) ; 
-
-  check(ssopt , setsockopt) ;  
-
-  int rc = bind(server_socket_fd, (SA *) &server, server_len) ;  
-  check(rc, bind) ;  
-
-  rc  = listen(server_socket_fd,  LISTEN_BACKLOG) ;  
-  check(rc, listen) ;  
-
-  http_reqhdr_t *http_header = nptr; 
-  char http_request_raw_buffer[HTTP_REQST_BUFF] ={ 0 };   
+  int pstatus =  EXIT_SUCCESS; 
+  setvbuf(stdout,  nptr , _IONBF , 0) ;  //!  No buffering on stdout 
+  htftp_t *hf  = htftp_start(9090,nptr, nptr) ;  
+  if (!hf) 
+  {
+     error(pstatus=EXIT_FAILURE, 0, "fail to start htftp\n"); 
+     goto  __prolog; 
+  }
+  
+  char http_request_raw_buffer[HTTP_REQST_BUFF] ={ 0 }; 
+  
   while (1)  
   {  
     bzero(http_request_raw_buffer , HTTP_REQST_BUFF) ;  
+    int  polling_status =  htftp_polling(hf);  
+    int  user_agent_socket =0 ; 
     
-    struct pollfd  sockpoll= { .fd=accept(server_socket_fd , NOACK) ,  POLLIN, 0}; 
-   
-    if (~0 == sockpoll.fd) 
+    if (~0 == polling_status) 
     {
        warnx("Poll init socket ACK  issue") ; 
        goto __http_restor ;  
     }
-
-    int  sockpoll_ready =  poll(&sockpoll ,1, ~0 /* Infinit  waiting  */); 
-   
-    //!Ignoring 0  return  by poll : see man page for more info 
-    if (~0 == sockpoll_ready)
-    {
-      warn("socket polling error %s \n" ,  strerror(*__errno_location())) ; 
-      goto __http_restor ; 
-    } 
-    
     /*Listen only on incomming data  */
-    if (sockpoll.revents & POLLIN) 
+    if ((polling_status >>8) & POLLIN) 
     { 
-      ssize_t  rbytes =  recv(sockpoll.fd ,  http_request_raw_buffer ,HTTP_REQST_BUFF, 0); 
+      user_agent_socket = (polling_status & 0xff) ;  
+      ssize_t  rbytes =  recv(user_agent_socket,http_request_raw_buffer ,HTTP_REQST_BUFF, 0); 
       if (!rbytes /* No data  */) 
         goto __http_restor ; 
       
       printf("%s\n" ,  http_request_raw_buffer) ; 
     }
 
-    http_header  = parse_http_request(http_request_raw_buffer) ;  
+    http_reqhdr_t * http_header  = parse_http_request(http_request_raw_buffer) ;  
     assert(http_header) ; 
      
     char *target_file = http_get_requested_content(http_header) ;   
@@ -85,7 +63,7 @@ main(int ac , char **av , char **env)
     bzero(http_request_raw_buffer , HTTP_REQST_BUFF) ; 
     char *request_content  = http_read_content(target_file, http_request_raw_buffer ) ;  
      
-    if (http_transmission(sockpoll.fd , request_content)) 
+    if (http_transmission(user_agent_socket , request_content)) 
       warnx("http transmission error") ; 
 
 
@@ -93,11 +71,11 @@ main(int ac , char **av , char **env)
       free(http_header) , http_header = 0 ; 
     
 __http_restor: 
-    close(sockpoll.fd) ;  
+    close(user_agent_socket) ;  
   }
 
-  shutdown(server_socket_fd , SHUT_RDWR) ; 
-  close(server_socket_fd) ; 
+  htftp_close(hf) ;  
 
-  return EXIT_SUCCESS ; 
+__prolog:
+  return pstatus ; 
 }
